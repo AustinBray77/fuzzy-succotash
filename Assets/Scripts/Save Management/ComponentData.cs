780f81d305d9bd767d582ce77a4a926763717516
deoignition <93416177+AustinBray77@users.noreply.github.com>
2023-03-11 00:50:38 -0500

    Finished save system

--- Assets/Scripts/Save Management/ComponentData.cs
+++ Assets/Scripts/Save Management/ComponentData.cs
@@ -5,12 +5,18 @@ using System.Runtime.Serialization;
 [Serializable]
 public class ComponentData
 {
-    private Dictionary<string, object> _data = new Dictionary<string, object>();
+    private Dictionary<string, object> _savedData = new Dictionary<string, object>();
+
+    public void SetValuePrimitive<T>(string name, T value) where T : struct
+        => _savedData.Add(name, value);
 
     public void SetValue<T>(string name, T value) where T : ISerializable
-        => _data.Add(name, value);
+        => _savedData.Add(name, value);
+
+    public T GetValuePrimitive<T>(string name) where T : struct
+        => (T)Convert.ChangeType(_savedData[name], typeof(T));
 
     public T GetValue<T>(string name) where T : ISerializable
-        => (T)Convert.ChangeType(_data[name], typeof(T));
+        => (T)Convert.ChangeType(_savedData[name], typeof(T));
 
 }
--- Assets/Scripts/Save Management/SaveHandler.cs
+++ Assets/Scripts/Save Management/SaveHandler.cs
@@ -12,14 +12,35 @@ public class SaveHandler : Singleton<SaveHandler>
     private Dictionary<int, SaveableComponent> _saveableComponents;
 
     //Methods for adding and removing saveable components for when they load in
-    public void AddSaveableComponent(SaveableComponent sc)
+    public void AddSaveableComponent(SaveableComponent saveableComponent)
     {
-        _saveableComponents.Add(sc.ID, sc);
+        _saveableComponents.Add(saveableComponent.ID, saveableComponent);
     }
 
-    public void RemoveSaveableComponent(SaveableComponent sc)
+    public void RemoveSaveableComponent(SaveableComponent saveableComponent)
     {
-        _saveableComponents.Remove(sc.ID);
+        _saveableComponents.Remove(saveableComponent.ID);
+    }
+
+    //Methods for overwriting data on a possibly existing component, and getting components from ID
+    public void SafeOverwriteComponent(SaveableComponent saveableComponent)
+    {
+        if (_saveableComponents.ContainsKey(saveableComponent.ID))
+        {
+            _saveableComponents[saveableComponent.ID] = saveableComponent;
+        }
+
+        AddSaveableComponent(saveableComponent);
+    }
+
+    public SaveableComponent GetSaveableComponent(int ID)
+    {
+        if (_saveableComponents.ContainsKey(ID))
+        {
+            return _saveableComponents[ID];
+        }
+
+        throw new Exception("Invalid Save Component ID");
     }
 
     //Method for saving data
@@ -32,16 +53,16 @@ public class SaveHandler : Singleton<SaveHandler>
         }
 
         //Recreates the save file
-        FileStream fs = File.Create(_test_file_path);
+        FileStream saveFileStream = File.Create(_saveFilePath);
         BinaryFormatter formatter = new BinaryFormatter();
 
         //Loops through and serializes each component
-        foreach (var component in _saveableComponents.Values)
+        foreach (var currentComponent in _saveableComponents.Values)
         {
             try
             {
                 //Serializes the data into the file
-                formatter.Serialize(fs, component.Serialize());
+                formatter.Serialize(saveFileStream, currentComponent.Serialize());
             }
             catch (Exception e)
             {
@@ -50,13 +71,33 @@ public class SaveHandler : Singleton<SaveHandler>
             }
         }
 
-
-        fs.Close();
+        //Closes the file
+        saveFileStream.Close();
     }
 
     //Method for loading data
     public void Load()
     {
+        //If the save file isnt found, throw an error
+        if (!File.Exists(_saveFilePath))
+        {
+            throw new Exception("Save File Not Found.");
+        }
 
+        //Opens the save file and formatter
+        FileStream saveFileStream = File.OpenRead(_saveFilePath);
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        //Deserialize the data
+        ComponentData[] components = (ComponentData[])formatter.Deserialize(saveFileStream);
+
+        //Reassign each value to its component
+        foreach (var currentComponent in components)
+        {
+            GetSaveableComponent(currentComponent.GetValuePrimitive<int>("ID")).Deserialize(currentComponent);
+        }
+
+        //Closes the file
+        saveFileStream.Close();
     }
 }
