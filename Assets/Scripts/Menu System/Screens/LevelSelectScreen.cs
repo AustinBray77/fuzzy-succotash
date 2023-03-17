c993991e7d1480a96106a37f23c75ab74e66ffa5
deoignition <93416177+AustinBray77@users.noreply.github.com>
2023-03-16 23:40:22 -0400

    Updates to the level select screen

--- Assets/Scripts/Menu System/Screens/LevelSelectScreen.cs
+++ Assets/Scripts/Menu System/Screens/LevelSelectScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,16 +28,23 @@ public class LevelSelectScreen : MonoBehaviour, IScreen
 
     public void GenerateLevelCards()
     {
-        int CardMargin = 25;
+        float CardMargin = 12.5f;
         int Rows = (int)(Screen.height / (LevelCard.CARD_HEIGHT + 2 * CardMargin));
         int CardsPerRow = (int)(Screen.width / (LevelCard.CARD_WIDTH + 2 * CardMargin));
 
         _levelCards = new LevelCard[Rows * CardsPerRow];
 
+        GameObject[] newScreenElements = new GameObject[_screenElements.Length + _levelCards.Length];
+        Array.Copy(_screenElements, newScreenElements, _screenElements.Length);
+
         for (int i = 0; i < Rows; i++)
             for (int j = 0; j < CardsPerRow; j++)
             {
                 int OneDimensionalIndex = i * CardsPerRow + j;
+
+                if (OneDimensionalIndex >= LevelHandler.Instance.LevelReferences.Length)
+                    break;
+
                 LevelCard currentCard = Instantiate(MenuController.Instance.Prefabs.LevelCard, transform).GetComponent<LevelCard>();
 
                 currentCard.GenerateFromLevel(LevelHandler.Instance.LevelReferences[OneDimensionalIndex].Data);
@@ -44,16 +52,20 @@ public class LevelSelectScreen : MonoBehaviour, IScreen
                 RectTransform cardTransform = currentCard.GetComponent<RectTransform>();
 
                 //Sets anchor to the top left
-                cardTransform.anchorMin = new Vector2(0, 0);
-                cardTransform.anchorMax = new Vector2(1, 1);
+                cardTransform.anchorMin = new Vector2(0, 1);
+                cardTransform.anchorMax = new Vector2(0, 1);
                 cardTransform.pivot = new Vector2(0, 0);
 
                 float xPos = j * (LevelCard.CARD_WIDTH + 2 * CardMargin) + CardMargin;
-                float yPos = i * (LevelCard.CARD_HEIGHT + 2 * CardMargin) + CardMargin;
+                float yPos = -((i + 1) * (LevelCard.CARD_HEIGHT + 2 * CardMargin) + CardMargin);
 
                 cardTransform.anchoredPosition = new Vector2(xPos, yPos);
 
                 _levelCards[OneDimensionalIndex] = currentCard;
+                newScreenElements[_screenElements.Length + OneDimensionalIndex] = currentCard.gameObject;
             }
+
+        _screenElements = newScreenElements;
+
     }
 }
